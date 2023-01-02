eace96f490cf6d4ee00a85b559252f35a3480ed3
yourskc <yourskc@yahoo.com.tw>
2023-01-02 21:43:24 +0800

    Initial

---
+++ Assets/show_cam.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using MoilDll;
+using TMPro;
+public class show_cam : MonoBehaviour {
+    public UnityEngine.UI.Image Rot_Image;
+    public Text Rot_Text;
+    public TextMeshProUGUI Tmp_Text;
+    Texture2D tex;
+    byte[] ByteArray;
+    private Sprite mySprite;
+	MemoryStream memoryStream;
+    float yaw, pitch;
+    Moil moil = new Moil();
+    float yaw_prev, pitch_prev;
+    void Start () {
+
+        float x = transform.eulerAngles.x;
+        x = 360f - x;
+        if (x == 360) x = 0;
+        float y = transform.eulerAngles.y;
+        float z = transform.eulerAngles.z;
+        Rot_Text.text = x.ToString("F3") + "    " + y.ToString("F3") + "    " + z.ToString("F3");
+        Tmp_Text.text = x.ToString("F3") + "    " + y.ToString("F3") + "    " + z.ToString("F3");
+
+        // 轉換
+        if (x >= 0 && x <= 90) pitch = x;
+        else if (x >= 270 && x <= 360) pitch = x - 360;
+        else pitch = 0;
+        if (y >= 0 && y <= 90) yaw = y;
+        else if (y >= 270 && y <= 360) yaw = y - 360;
+        else yaw = 0;
+
+        ByteArray = moil.process(yaw, pitch, 4);
+        yaw_prev = yaw;
+        pitch_prev = pitch;
+
+        memoryStream = new MemoryStream(5000000);
+		memoryStream.Write(ByteArray, 0, ByteArray.Length);
+		memoryStream.Position = 0;
+        // tex = new Texture2D(1920, 1440);
+        tex = new Texture2D(960, 720);
+        tex.LoadImage(ByteArray);
+        mySprite = Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0.5f, 0.5f), 50.0f);
+        Rot_Image.sprite = mySprite;
+        
+    }
+	
+
+	void Update () {
+
+        float x = transform.eulerAngles.x;
+        x = 360f - x;
+        if (x == 360) x = 0;
+        float y = transform.eulerAngles.y;
+        float z = transform.eulerAngles.z;
+        Rot_Text.text = x.ToString("F3") + "    " + y.ToString("F3") + "    " + z.ToString("F3");
+        Tmp_Text.text = x.ToString("F3") + "    " + y.ToString("F3") + "    " + z.ToString("F3");
+
+        // 轉換
+        if (x >= 0 && x <= 90) pitch = x;
+        else if (x >= 270 && x <= 360) pitch = x - 360;
+        else pitch = 0;
+        if (y >= 0 && y <= 90) yaw = y;
+        else if (y >= 270 && y <= 360) yaw = y - 360;
+        else yaw = 0;
+        if (yaw != yaw_prev || pitch != pitch_prev)
+        {
+            ByteArray = moil.process(yaw, pitch, 4);
+            yaw_prev = yaw;
+            pitch_prev = pitch;
+        }
+        memoryStream.Write(ByteArray, 0, ByteArray.Length);
+		memoryStream.Position = 0;		
+		
+        // tex = new Texture2D(640, 480);
+        tex.LoadImage(ByteArray);
+		mySprite = Sprite.Create(tex, new Rect(0.0f, 0.0f, 960, 720), new Vector2(0.5f, 0.5f), 50.0f);
+		Rot_Image.sprite = mySprite;
+        
+
+    }
+ 
+}
--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # UnityMoil
-UnityMoil
+
+
+
+
+1. Create a new Unity Project
+
+2. Put Lab.jpg under Project 
+
+3. Copy to the following files to  Assets\ folder
+
+MoilDll.dll
+MoilDll.pdb
+Emgu.CV.World.dll
+show_cam.cs   
+cvextern.dll
+ 
+4. Add some components as the following, adjust size and positions.
+ 
+GameObject/ UI/ canvas
+
+GameObject/ UI/ Panel (Disable)
+
+GameObject/ UI/ Image
+
+PosX=240, PosY=-145
+Width=200, Height=160
+ScaleX=3, ScaleY=3
+
+GameObject/ UI/ Text(TMP)
+
+PosX= -150, PosY=-200
+Width=500, Height= 30
+Text=MOIL
+Font Size=24
+Color = Red
+
+5. Select Main Camera, Component/ Scripts/ Select Show_cam 
+
+6. Drag previous Image and Text to it's properities  
+
+7. Press Play button, Hold Mouse right button and try to control 
+
+the view angle of the small window.    
+
+
